12c3a1a82b7d39953f9f05f19bb42baadeeb1565
Daisuke Maki <lestrrat+github@gmail.com>
2022-06-01 13:25:21 +0900

    Add NewBuilder() for convenience

--- resource/resource.go
+++ resource/resource.go
@@ -15,7 +15,18 @@ func defaultUserValidate(v *User) error {
 	return nil
 }
 
+// Builder is a centralized store for other type-specific builders,
+// which exists mainly for convenience. Its zero-value can be used
+// safely, but you could also use the `resource.NewBuilder()` function
+// to start a method calling chain.
 type Builder struct{}
+
+// NewBuilder creates a new instance of the Builder object.
+// This method exists solely as convenience, as the zero-value for
+// the `resource.Builder` can safely be used without any initialization
+func NewBuilder() *Builder {
+	return &Builder{}
+}
 
 type AuthenticationSchemeType string
 

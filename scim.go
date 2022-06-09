a1db16397e4e16d7550f0899fb94b67e4c89c472
Daisuke Maki <lestrrat+github@gmail.com>
2022-06-09 15:57:57 +0900

    chain more go generate calls

--- scim.go
+++ scim.go
@@ -1,4 +1,7 @@
 //go:generate ./tools/cmd/genoptions.sh
 //go:generate go generate ./filter
+//go:generate go generate ./resource
+//go:generate go generate ./sample
+//go:generate go generate ./client
 
 package scim
